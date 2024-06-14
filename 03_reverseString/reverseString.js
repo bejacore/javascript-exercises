7df0416605ca60ca74986b7f795b79ed181c4134
bejacore <bejacore@gmail.com>
2024-06-14 15:50:35 -0500

    03_reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = '';
+    let wordLenght = word.length;
 
+    for (let i = 1; i <= wordLenght; i++) {
+        reversedWord += word.substr(-i, 1);
+    } 
+
+    return reversedWord;
 };
 
 // Do not edit below this line
