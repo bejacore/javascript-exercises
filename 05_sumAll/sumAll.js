237144ec8bdf031fe19e65a1214a1a1cd3f07127
bejacore <bejacore@gmail.com>
2024-06-18 14:21:53 -0500

    05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    args.sort();
 
+    let sum = 0;
+    const initialNum = args.at(0);
+    const finalNum = args.at(-1);
+
+    if (typeof initialNum !== 'number' || 
+        typeof finalNum !== 'number' ||
+        initialNum < 0) {
+
+        return 'ERROR';
+    }
+
+    for (let num = initialNum; num <= finalNum; num++) {
+        sum += num;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
