ac28bd74ab04b1b09d63ecf4f1e185e61a556bae
bejacore <bejacore@gmail.com>
2024-06-18 13:35:41 -0500

    04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    const arrayLength = array.length;
+
+    for (const itemToRemove of itemsToRemove) {
+
+        let itemToRemoveCount = 0;
+        for (const item of array) {
+            if (item === itemToRemove) {
+                itemToRemoveCount++;
+            }
+        }
+
+        for (let count = 0; count < itemToRemoveCount; count++) {
+            for (let index = 0; index < arrayLength; index++) {
+                let item = array[index];
+    
+                if (item === itemToRemove) {
+                    array.splice(index, 1);
+                }
+            }
+        }
+
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
