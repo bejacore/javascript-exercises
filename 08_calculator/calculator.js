498fd873b44a1dbb8e28cd6f7a2114e2c96f19ab
bejacore <bejacore@gmail.com>
2024-07-11 19:40:39 -0500

    Add 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  } else {
+    let total = 1;
+    for (let i = num; i > 0; i--) {
+      total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
