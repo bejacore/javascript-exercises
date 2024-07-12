6c7c5867cb236e691d8cd13abb56dcdaf6ffcd1f
bejacore <bejacore@gmail.com>
2024-07-12 15:01:14 -0500

    Add 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    let fib = [0, 1];
+    if (num === 0) {
+        return fib[num];
+    } else if (num < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 2; i <= num; i++) {
+            fib.push(fib[i - 1] + fib[i - 2]);
+        }
+        return fib.at(-1)
+    }
 };
 
 // Do not edit below this line
