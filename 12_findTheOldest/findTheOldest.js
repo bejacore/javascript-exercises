c3496d8f870b9ce40208e8975a94dca933d50682
bejacore <bejacore@gmail.com>
2024-07-12 16:09:17 -0500

    Add 12_findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    persons = persons.map(person => {
+        if (!('yearOfDeath' in person)) {
+            let date = new Date();
+            person['yearOfDeath'] = date.getFullYear();
+            return person;
+        }
+        return person;
+    });
 
+    return persons.sort((a, b) => {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge - aAge;
+    })[0]
 };
 
 // Do not edit below this line
